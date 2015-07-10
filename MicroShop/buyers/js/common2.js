1359a022014e83d2ad924b128d564de90039c01f
fox <626269256@qq.com>
2015-07-10 14:14:17 +0800

    【提交】common.js

---
+++ MicroShop/buyers/js/common2.js
@@ -0,0 +1,6 @@
+// 公用JS
+window.onload = window.onresize = function(){
+	var _html = document.getElementsByTagName('html')[0];
+	var view_width = _html.getBoundingClientRect().width;
+	view_width>640?_html.style.fontSize = 640 / 16 + 'px':_html.style.fontSize = view_width / 16 + 'px';
+}
