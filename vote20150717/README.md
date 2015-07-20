39bd59c42c8e9c0e1426c8bd89595fb8f8643977
fox <626269256@qq.com>
2015-07-20 11:58:47 +0800

    【增加】投票页面项目

---
+++ vote20150717/README.md
@@ -0,0 +1,5 @@
+# vote20150717
+> 投票手机端页面（赛亚人）
+> activity_description.html -- 活动说明
+> votes.html                -- 投票页
+> ranking_list.html         -- 排行榜
---
+++ vote20150717/js/main.js
@@ -0,0 +1,14 @@
+/**
+ * 投票页面所需的js
+ * @authors 626269256@qq.com
+ * @date    2015-07-18 13:28:11
+ * @version $Id$
+ */
+
+(function (){
+
+	// 返回
+	$('#header .back').on('click',function (){
+		history.back();
+	});
+})();
