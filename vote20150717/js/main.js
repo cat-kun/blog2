4f3f50a2bb95e580c78323053af289a4b0267b4e
fox <626269256@qq.com>
2015-07-23 10:44:53 +0800

    【完成】vote20150717项目的交互

--- vote20150717/js/main.js
+++ vote20150717/js/main.js
@@ -1,8 +1,8 @@
 /**
- * 投票页面所需的js
+ * 投票页面,排行榜所需的js
  * @authors 626269256@qq.com
  * @date    2015-07-18 13:28:11
- * @version $Id$
+ * @version 1.0
  */
 
 (function (){
@@ -10,5 +10,14 @@
 	// 返回
 	$('#header .back').on('click',function (){
 		history.back();
+	});
+
+	// 投票列表交互
+	$('.nav a').on('click',function (){
+		var $ul = $(this).parent().siblings('.mainCont').find('ul');
+		$(this).addClass('current').siblings().removeClass('current');
+
+		$ul.hide();
+		$ul.eq( $(this).index() ).show();
 	});
 })();
