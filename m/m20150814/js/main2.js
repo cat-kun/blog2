205b857a8f50e189db556c9af3ae2d590c5a8da7
fox <626269256@qq.com>
2015-08-18 09:41:40 +0800

    修复 手机3G访问时，第1次访问第1个页面没动画效果的，关掉浏览器第2次访问，就有动画效果

--- m/m20150814/js/main2.js
+++ m/m20150814/js/main2.js
@@ -50,59 +50,57 @@ var imgs = [
 Preload.loadImg(imgs,function (){
 	console.log('图片加载完成');
 	$('.swiper-slide0').hide();
-});
+
 // setTimeout(function (){$('.swiper-slide0').hide();},20000);
-var mySwiper = new Swiper('.swiper-container', {
-	direction: 'vertical',
-	//pagination: '.swiper-pagination',
-	//virtualTranslate : true,
-	onInit: function(swiper) {
-		swiperAnimateCache(swiper);
-		swiperAnimate(swiper);
-	},
-	onSlideChangeEnd: function(swiper) {
-		swiperAnimate(swiper);
-	},
-	onTransitionEnd: function(swiper) {
-		swiperAnimate(swiper);
-		var mmH = $('.imgBox>img').height();
-		$('.imgBox').css('height',mmH);
-		
-	},
+	var mySwiper = new Swiper('.swiper-container', {
+		direction: 'vertical',
+		//pagination: '.swiper-pagination',
+		//virtualTranslate : true,
+		onInit: function(swiper) {
+			swiperAnimateCache(swiper);
+			swiperAnimate(swiper);
+		},
+		onSlideChangeEnd: function(swiper) {
+			swiperAnimate(swiper);
+		},
+		onTransitionEnd: function(swiper) {
+			swiperAnimate(swiper);
+			var mmH = $('.imgBox>img').height();
+			$('.imgBox').css('height',mmH);
+			
+		},
 
-	onSlideChangeEnd: function(swiper){
-		var slide = $('.swiper-slide');
-		if(mySwiper.isEnd){
-			$('#array').hide();
-		}else{
-			$('#array').show();
+		onSlideChangeEnd: function(swiper){
+			var slide = $('.swiper-slide');
+			if(mySwiper.isEnd){
+				$('#array').hide();
+			}else{
+				$('#array').show();
+			}
+		},
+
+		watchSlidesProgress: true,
+
+		onProgress: function(swiper) {
+			for (var i = 0; i < swiper.slides.length; i++) {
+				var slide = swiper.slides[i];
+				var progress = slide.progress;
+				var translate = progress * swiper.height / 4;
+				scale = 1 - Math.min(Math.abs(progress * 0.5), 1);
+				var opacity = 1 - Math.min(Math.abs(progress / 2), 0.5);
+				slide.style.opacity = opacity;
+				es = slide.style;
+				es.webkitTransform = es.MsTransform = es.msTransform = es.MozTransform = es.OTransform = es.transform = 'translate3d(0,' + translate + 'px,-' + translate + 'px) scaleY(' + scale + ')';
+
+			}
+		},
+
+		onSetTransition: function(swiper, speed) {
+			for (var i = 0; i < swiper.slides.length; i++) {
+				es = swiper.slides[i].style;
+				es.webkitTransitionDuration = es.MsTransitionDuration = es.msTransitionDuration = es.MozTransitionDuration = es.OTransitionDuration = es.transitionDuration = speed + 'ms';
+
+			}
 		}
-	},
-
-	watchSlidesProgress: true,
-
-	onProgress: function(swiper) {
-		for (var i = 0; i < swiper.slides.length; i++) {
-			var slide = swiper.slides[i];
-			var progress = slide.progress;
-			var translate = progress * swiper.height / 4;
-			scale = 1 - Math.min(Math.abs(progress * 0.5), 1);
-			var opacity = 1 - Math.min(Math.abs(progress / 2), 0.5);
-			slide.style.opacity = opacity;
-			es = slide.style;
-			es.webkitTransform = es.MsTransform = es.msTransform = es.MozTransform = es.OTransform = es.transform = 'translate3d(0,' + translate + 'px,-' + translate + 'px) scaleY(' + scale + ')';
-
-		}
-	},
-
-	onSetTransition: function(swiper, speed) {
-		for (var i = 0; i < swiper.slides.length; i++) {
-			es = swiper.slides[i].style;
-			es.webkitTransitionDuration = es.MsTransitionDuration = es.msTransitionDuration = es.MozTransitionDuration = es.OTransitionDuration = es.transitionDuration = speed + 'ms';
-
-		}
-	},
-
-
-
-})
+	});
+});
