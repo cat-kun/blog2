42cb94c105cfe7f42b6cf3135108992e07ae2758
fox <626269256@qq.com>
2015-08-22 10:12:42 +0800

    添加m20150818测试文件

---
+++ m/m20150818/js/fontSize.js
@@ -0,0 +1,9 @@
+// 设置html页面根节点字体大小
+var _html,view_width;
+fontSize();
+window.onresize = fontSize;
+function fontSize(){
+	_html = document.getElementsByTagName('html')[0];
+	view_width = _html.getBoundingClientRect().width;
+	view_width>720?_html.style.fontSize = 720 / 16 + 'px':_html.style.fontSize = view_width / 16 + 'px';
+}
